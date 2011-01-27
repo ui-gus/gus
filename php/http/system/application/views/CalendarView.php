f4ab029f325c7b7f11a606ccfd1a9536a68653f7
Zeke Long <long3841@vandals.uidaho.edu>
2011-01-27 00:42:22 -0800

    CalendarView fix

--- php/http/system/application/views/CalendarView.php
+++ php/http/system/application/views/CalendarView.php
@@ -3,26 +3,27 @@
 <!--- written by Zeke Long --->
 
 <!--- display the calendar in month view --->
+<!--- STILL NEED TO IMPLEMENT WEEK VIEW --->
 <?php
+
 //Get today's date
 $date =time(); 
+
 //Put the day, month, and year in seperate variables 
 $day = date('d', $date); 
 $month = date('n', $date); 
 $year = date('Y', $date); 
 
-//Get the first day of the month 
-$first_day = mktime(0,0,0,$month,1,$year);
+//Get first day of the month 
+$first_day = mktime(0, 0, 0, $month, 1, $year);
 
 //Get the month name 
 $title = date("F", $first_day); 
 
 //Get the actual weekday that the first of the month is on 
-$day_of_week = date("l", $first_day) ; 
+$day_of_week = date("l", $first_day); 
 
-/*Once we know what day of the week it falls on, we know how 
-  many blank days occure before it. If the first day of the week 
-  is a Sunday then it would be zero 
+/*Once we know what day of the week it falls on, we know how many blank days occur before it (zero if Sunday).
 */
 switch($day_of_week)
 { 
@@ -35,29 +36,29 @@ switch($day_of_week)
 	case "Saturday": $blank = 6; break; 
 } 
 //Determine how many days are in the current month 
-$days_in_month = cal_days_in_month(0, $month, $year) ;
+$days_in_month = cal_days_in_month(0, $month, $year);
 
 //Start building the table heads
+
  echo "<table border=1 width=294>";
  echo "<tr><th colspan=7> $title $year </th></tr>";
- echo "<tr><td width=42>S</td><td width=42>M</td><td
- width=42>T</td><td width=42>W</td><td width=42>T</td><td
- width=42>F</td><td width=42>S</td></tr>";
+ echo "<tr><td width=42>Sun</td><td width=42>Mon</td><td
+ width=42>Tue</td><td width=42>Wed</td><td width=42>Thu</td><td
+ width=42>Fri</td><td width=42>Sat</td></tr>";
 
- //This counts the days in the week, up to 7
- $day_count = 1;
+ $day_count = 1;      //goes up to 7
  echo "<tr>";
 
- //Take care of the blank days
- while($blank > 0)
+ //take care of blank days
+ while ( $blank > 0 )
  {
  	echo "<td></td>";
- 	$blank = $blank-1;
+ 	$blank = $blank - 1;
 	$day_count++;
  } 
-$day_num = 1;                //set the first day of the month to 1 
-//count up the days, until we've done all of them in the month 
-while($day_num <= $days_in_month) 
+$day_num = 1;            //set first day of month to 1 
+//display all days in the month 
+while ($day_num <= $days_in_month) 
 {
 	echo "<td> $day_num </td>"; 
 	$day_num++; $day_count++; 
@@ -68,8 +69,8 @@ while($day_num <= $days_in_month)
 	} 
 }
 
-//Finaly we finish out the table with some blank details if needed 
-while($day_count > 1 && $day_count <= 7) 
+//Finish the table with blank details if needed 
+while ( $day_count >1 && $day_count <=7 ) 
 {
 	echo "<td> </td>"; 
 	$day_count++; 
@@ -81,7 +82,8 @@ echo "</tr></table>";
 <!--- form to add an event to calendar --->
 <form action="calendar.php" method="add_event">
 	Event Description: <input type="text" name="event"/> <br>
-	Date:<select name="month">
+
+	Date: <select name="month">          //month dropdown
 		<option value="January">January</option>
 		<option value="February">February</option>
 		<option value="March">March</option>
@@ -95,7 +97,8 @@ echo "</tr></table>";
 		<option value="November">November</option>
 		<option value="December">December</option>
 	</select>
-	<select name="day">
+
+	<select name="day">                    //day dropdown
 		<option value="1">1</option>
 		<option value="2">2</option>
 		<option value="3">3</option>
@@ -127,7 +130,11 @@ echo "</tr></table>";
 		<option value="29">29</option>
 		<option value="30">30</option>
 		<option value="31">31</option>
-	</select> <br>
+	</select>
+
+	Year (mm/dd/yyyy): <input type="integer" name="year"/>
+	<br>
+
 	Time: <select name="hour">
 		<option value="12">12</option>
 		<option value="1">1</option>
