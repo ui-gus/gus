3a7f8d324d1beb880ca6868548af430850222482
cblair <cblair@vandals.uidaho.edu>
2011-01-27 18:56:34 -0800

    Housekeeping on home controller, added som configs in database.

--- php/http/system/application/controllers/home.php
+++ php/http/system/application/controllers/home.php
@@ -22,15 +22,4 @@ class Home extends Controller {
 		$this->load->view('home',$this->pdata);
 	}
 
-	function groupview()
-	{
-		//set page name
-		$page_name = "home";
-		if(func_num_args() > 0) {$page_name = func_get_arg(0);}
-		$this->pdata['header'] = $this->Page->get_header($page_name);
-		$this->pdata['content'] = $this->Page->get_content($page_name);
-		//footer already set
-		$this->load->view('main',$this->pdata);
-	}
-
 }
