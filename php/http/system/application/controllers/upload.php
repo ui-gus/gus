3fcd51a751bbe4f238fed5b1417c9fd3e2103950
Alex Nilson <alexnilson@vandals.uidaho.edu>
2011-01-28 16:23:11 -0800

    Added a directory for uploaded files in php/http. Added files that handle uploading of files

---
+++ php/http/system/application/controllers/upload.php
@@ -0,0 +1,43 @@
+<?php
+
+class Upload extends Controller {
+
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->helper(array('form', 'url'));
+	}
+
+	function index()
+	{
+		$this->load->view('upload_form', array('error' => ' ' ));
+	}
+
+	function do_upload()
+	{
+		$config['upload_path'] = './uploads/';
+		$config['allowed_types'] = 'gif|jpg|png|txt|png|bmp|doc|pdf';
+		$config['max_size']	= '0';
+		$config['max_width']  = '0';
+		$config['max_height']  = '0';
+		$config['overwrite']  = 'TRUE';
+		$config['remove_spaces']  = 'TRUE';
+
+
+		$this->load->library('upload', $config);
+
+		if ( ! $this->upload->do_upload())
+		{
+			$error = array('error' => $this->upload->display_errors());
+
+			$this->load->view('upload_form', $error);
+		}
+		else
+		{
+			$data = array('upload_data' => $this->upload->data());
+
+			$this->load->view('upload_success', $data);
+		}
+	}
+}
+?>
