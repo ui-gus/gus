73165cb4a88e0ee467c1f9d101fe31bf0f6225be
brett <brett@brett-desktop.(none)>
2011-02-03 21:32:49 -0800

    Adding user / group page user tests.

--- php/http/system/application/controllers/grouppage.php
+++ php/http/system/application/controllers/grouppage.php
@@ -2,12 +2,27 @@
 
 class Grouppage extends Controller {
 
-	function Template(){
+	function Grouppage(){
 		parent::Controller();
+		$this->load->model('Page');
+		$this->load->helper('url');
 	}
 	
 	function index() {
-		echo "Oh HERRO.";
+		$data['title'] = "Group Page";
+		$data['heading'] = "Group Page";
+
+		$data['header'] = $this->Page->get_header('groups');
+		$data['content'] = $this->Page->get_content('groups');
+		$data['footer'] = $this->Page->get_footer();				
+			
+		$this->load->view( 'grouppage_view.php', $data );
+
+		//set page name
+		$this->load->library('unit_test');
+                echo $this->unit->run('Gus Groups.',$this->Page->get_content('groups'), 'Userpage test');
+		//$this->load->view('test', $this->pdata);	
+				
 	}
 
 }
