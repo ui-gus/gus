f02c78d9c8682c6ca5dff49f020195a0009e5c4b
Cynthia Rempel <remp6007@wormulon.cs.uidaho.edu>
2011-02-14 17:54:40 -0800

    Adding basic docstrings

--- php/http/system/application/controllers/pages.php
+++ php/http/system/application/controllers/pages.php
@@ -1,12 +1,12 @@
 <?php
+/**
+ * @package GusPackage
+ * subpackage Pages
+ * @author Colby Blair <cblair@vandals.uidaho.edu>
+ * @version 0.4
+ * @copyright University of Idaho 2011
+ */
 
-/******************************************************************************
-*Gus - Groups in a University Setting
- University of Idaho CS 384 - Spring 2011
- GusPHP Subteam
- File Authors:
-                Colby Blair
-******************************************************************************/
 
 class Pages extends Controller {
 	var $pdata; //page data
