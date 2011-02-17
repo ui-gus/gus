ffe398fdb229837270e381a07c1443a1b38d7fc5
cblair <cblair@vandals.uidaho.edu>
2011-02-17 10:30:12 -0800

    Fixed my user add db issue.

--- php/db/gusphp.sql
+++ php/db/gusphp.sql
@@ -147,7 +147,7 @@ DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `un` varchar(100) NOT NULL,
   `pw` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
