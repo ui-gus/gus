b8338bcfcc8f26c4fedfbc5830dde408e0f95bcc
laur6383 <User@.(none)>
2011-03-01 23:17:03 -0800

    trying to update forum views

--- php/http/system/application/views/edit_thread_view.php
+++ php/http/system/application/views/edit_thread_view.php
@@ -9,41 +9,32 @@
 </head>
 
 <body>
-
-
-<table width="400" border="0" align="center" cellpadding="0" cellspacing="1" bgcolor="#CCCCCC">
-<tr>
-<?php echo form_open('forum/thread_update');?>
-<?php echo form_hidden('id', $this->fdata['id']);?>
-<td>
-<table width="100%" border="0" cellpadding="3" cellspacing="1" bgcolor="#FFFFFF">
-<tr>
-<td colspan="3" bgcolor="#E6E6E6"><strong>Edit Topic</strong> </td>
-</tr>
-
-<tr>
-<td width="14%"><strong>Title</strong></td>
-<td width="84%"><input type="text" name="topic" value= "<?php	echo $this->fdata['topic']; ?>" size="80" /></td>
-</tr>
-
-<tr>
-<td valign="top"><strong>Body</strong></td>
-<td><textarea name="body" cols="70" rows="3"><?php	echo $this->fdata['body']; ?></textarea></td>
-</tr>
-
-
-<tr>
-<td>&nbsp;</td>
-<td><input type="submit" value="Submit" /> <input type="reset" value="Reset" /></td>
-</tr>
-
-</table>
-</td>
-</form>
-</tr>
-</table>
-
-
+	<table width="400" border="0" align="center" cellpadding="0" cellspacing="1" bgcolor="#CCCCCC">
+		<tr>
+			<?php echo form_open('forum/thread_update');?>
+			<?php echo form_hidden('thread_id', $this->fdata['id']);?>
+			<td>
+				<table width="100%" border="0" cellpadding="3" cellspacing="1" bgcolor="#FFFFFF">
+					<tr>
+						<td colspan="3" bgcolor="#E6E6E6"><strong>Edit Topic</strong> </td>
+					</tr>
+					<tr>
+						<td width="14%"><strong>Title</strong></td>
+						<td width="84%"><input type="text" name="thread_topic" value= "<?php echo $this->fdata['topic']; ?>" size="80" /></td>
+					</tr>
+					<tr>
+						<td valign="top"><strong>Body</strong></td>
+						<td><textarea name="thread_body" cols="70" rows="3"><?php	echo $this->fdata['body']; ?></textarea></td>
+					</tr>
+					<tr>
+						<td>&nbsp;</td>
+						<td><input type="submit" value="Submit" /> <input type="reset" value="Reset" /></td>
+					</tr>
+				</table>
+			</td>
+			</form>
+		</tr>
+	</table>
 </body>
 
 </html>
--- php/http/system/application/views/forum_view.php
+++ php/http/system/application/views/forum_view.php
@@ -1,45 +1,33 @@
 <?php echo $this->pdata['header'] ?>
-
 <?php 	echo $this->pdata['content'] ?>
 
 
 <html>
-
-  
-
-<table width="90%" border="0" align="center" cellpadding="3" cellspacing="1" bgcolor="#CCCCCC">
-<tr>
-
-<td width="59%" align="center" bgcolor="#E6E6E6"><strong>Topic</strong></td>
-<td width="13%" align="center" bgcolor="#E6E6E6"><strong>Replies</strong></td>
-<td width="13%" align="center" bgcolor="#E6E6E6"><strong>Date/Time</strong></td>
-</tr>
-
-<?php foreach($this->pdata['query']->result() as $row): ?>
-
-	<tr>
-	<td bgcolor="#FFFFFF"><?php echo anchor('forum/view_thread/' .$row->id, $row->topic); ?></td>
-	<td align="center" bgcolor="#FFFFFF"><?php echo $row->reply; ?></td>
-	<td align="center" bgcolor="#FFFFFF"><?php echo $row->datetime; ?></td>
-	</tr>
-
-<?php endforeach; ?>
-
-<tr>
-<?php echo form_open('forum/create_thread');?>
-<td width="50%" align="left" bgcolor="#E6E6E6"><input type="submit" value="Search Forum" /td>
-</form>
-<td bgcolor="#E6E6E6"></td>
-<?php echo form_open('forum/create_thread');?>
-<td width="50%" align="right" bgcolor="#E6E6E6"><input type="submit" value="Create New Thread" /td>
-</form>
-
-</tr>
-
-</table>
-
+<body>
+	<table width="90%" border="0" align="center" cellpadding="3" cellspacing="1" bgcolor="#CCCCCC">
+		<tr>
+			<td width="59%" align="center" bgcolor="#E6E6E6"><strong>Topic</strong></td>
+			<td width="13%" align="center" bgcolor="#E6E6E6"><strong>Replies</strong></td>
+			<td width="13%" align="center" bgcolor="#E6E6E6"><strong>Date/Time</strong></td>
+		</tr>
+		<?php foreach($this->fdata['query']->result() as $row): ?>
+		<tr>
+			<td bgcolor="#FFFFFF"><?php echo anchor('forum/view_thread/' .$row->thread_id, $row->thread_topic); ?></td>
+			<td align="center" bgcolor="#FFFFFF"><?php echo $row->num_replies; ?></td>
+			<td align="center" bgcolor="#FFFFFF"><?php echo $row->datetime; ?></td>
+		</tr>
+		<?php endforeach; ?>
+		<tr>
+			<?php echo form_open('forum/search_forum');?>
+				<td width="50%" align="left" bgcolor="#E6E6E6"><input type="submit" value="Search Forum" /td>
+			</form>
+			<td bgcolor="#E6E6E6"></td>
+			<?php echo form_open('forum/create_thread');?>
+				<td width="50%" align="right" bgcolor="#E6E6E6"><input type="submit" value="Create New Thread" /td>
+			</form>
+		</tr>
+	</table>
 </body>
-
 </html>
 
 <?php echo $this->pdata['footer'] ?>
