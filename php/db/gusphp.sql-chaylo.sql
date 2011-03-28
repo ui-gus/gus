bcb33ce48e08bf5680983eb4444d583ba4c426c6
laur6383 <User@.(none)>
2011-03-28 16:12:28 -0700

    updated my database file

--- php/db/gusphp.sql-chaylo.sql
+++ php/db/gusphp.sql-chaylo.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Feb 15, 2011 at 08:56 PM
+-- Generation Time: Mar 28, 2011 at 03:39 PM
 -- Server version: 5.5.8
 -- PHP Version: 5.3.5
 
@@ -26,20 +26,29 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 --
 
 CREATE TABLE IF NOT EXISTS `calendar` (
-  `user` varchar(100) NOT NULL,
+  `user` varchar(32) NOT NULL,
   `date` date NOT NULL,
-  `data` text NOT NULL,
-  PRIMARY KEY (`date`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `data` varchar(150) NOT NULL,
+  `eventID` int(11) NOT NULL AUTO_INCREMENT,
+  KEY `eventID` (`eventID`),
+  KEY `eventID_2` (`eventID`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
 
 --
 -- Dumping data for table `calendar`
 --
 
-INSERT INTO `calendar` (`user`, `date`, `data`) VALUES
-('admin', '2011-02-15', 'this is an event'),
-('cblair', '2011-02-07', 'test'),
-('cblair', '2011-02-08', 'test');
+INSERT INTO `calendar` (`user`, `date`, `data`, `eventID`) VALUES
+('admin', '2011-02-26', 'another event', 3),
+('admin', '2011-02-24', 'updated', 2),
+('admin', '2011-02-21', 'event', 4),
+('user1', '2011-03-15', 'an event in march', 6),
+('admin', '2011-02-23', 'updated', 7),
+('admin', '0000-00-00', 'different event', 8),
+('long3841', '2011-02-19', 'event', 9),
+('cblair', '2011-02-21', 'asdfasdf', 10),
+('cblair', '2011-02-27', 'asdfasfdasfd', 11),
+('long3841', '2011-02-21', 'something', 12);
 
 -- --------------------------------------------------------
 
@@ -52,14 +61,19 @@ CREATE TABLE IF NOT EXISTS `ggroup` (
   `name` varchar(100) DEFAULT NULL,
   `description` text,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
 
 --
 -- Dumping data for table `ggroup`
 --
 
 INSERT INTO `ggroup` (`id`, `name`, `description`) VALUES
-(1, 'test', 'test2');
+(1, 'test', 'test2'),
+(5, 'test_group', 'test_desc'),
+(6, 'Testgroup', 'A test group.\n'),
+(7, 'test1', 'A group.\n'),
+(8, 'test2', 'Another group.'),
+(10, 'main', 'Gus main group. Users set to admin (7) on this group get system wide admin privileges.');
 
 -- --------------------------------------------------------
 
@@ -83,7 +97,7 @@ CREATE TABLE IF NOT EXISTS `messages` (
   `to_ddate` datetime DEFAULT NULL,
   `created` datetime NOT NULL,
   PRIMARY KEY (`userid`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
 -- Dumping data for table `messages`
@@ -92,7 +106,8 @@ CREATE TABLE IF NOT EXISTS `messages` (
 INSERT INTO `messages` (`userid`, `title`, `message`, `from`, `to`, `from_viewed`, `to_viewed`, `from_deleted`, `to_deleted`, `from_vdate`, `to_vdate`, `from_ddate`, `to_ddate`, `created`) VALUES
 (1, 'test', 'test', 1, 0, 0, 0, 0, 0, NULL, NULL, NULL, NULL, '2011-02-10 20:13:09'),
 (2, 'test2', 'test', 1, 0, 0, 0, 0, 0, NULL, NULL, NULL, NULL, '2011-02-10 20:13:30'),
-(3, 'test3', 'Hi', 1, 0, 0, 0, 0, 0, NULL, NULL, NULL, NULL, '2011-02-10 20:22:03');
+(3, 'test3', 'Hi', 1, 0, 0, 0, 0, 0, NULL, NULL, NULL, NULL, '2011-02-10 20:22:03'),
+(4, 'test', 'test', 1, 0, 0, 0, 0, 0, NULL, NULL, NULL, NULL, '2011-02-19 15:11:04');
 
 -- --------------------------------------------------------
 
@@ -105,14 +120,16 @@ CREATE TABLE IF NOT EXISTS `page` (
   `name` varchar(100) DEFAULT NULL,
   `content` text,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=16 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=19 ;
 
 --
 -- Dumping data for table `page`
 --
 
 INSERT INTO `page` (`id`, `name`, `content`) VALUES
-(15, 'test', 'Test content');
+(15, 'test', 'Test content'),
+(16, 'home', 'Gus Home Page.\n<br />\n\nThis is the latest (stablest) version of the project.\n<br />\n\ngit pull on 02/23/11 . Some stuff broke.'),
+(18, 'auth', 'Gus Authentication');
 
 -- --------------------------------------------------------
 
@@ -122,23 +139,29 @@ INSERT INTO `page` (`id`, `name`, `content`) VALUES
 
 CREATE TABLE IF NOT EXISTS `replies` (
   `thread_id` int(4) NOT NULL DEFAULT '0',
-  `reply_id` int(4) NOT NULL DEFAULT '0',
+  `reply_id` int(4) NOT NULL AUTO_INCREMENT,
   `author` varchar(65) NOT NULL DEFAULT '',
   `a_email` varchar(65) NOT NULL DEFAULT '',
   `body` longtext NOT NULL,
   `datetime` varchar(25) NOT NULL DEFAULT '',
   KEY `a_id` (`reply_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
 
 --
 -- Dumping data for table `replies`
 --
 
 INSERT INTO `replies` (`thread_id`, `reply_id`, `author`, `a_email`, `body`, `datetime`) VALUES
-(5, 0, 'Chaylo', '', 'Just testing the reply counter', '15/Feb/2011 07:59:36'),
-(5, 0, 'testing', '', 'testing some more', '15/Feb/2011 08:50:23'),
-(5, 0, 'testing', '', 'testing', '15/Feb/2011 08:53:16'),
-(6, 0, 'tesing', '', 'testing', '15/Feb/2011 08:53:27');
+(5, 1, 'Chaylo', '', 'Just testing the reply counter', '15/Feb/2011 07:59:36'),
+(5, 2, 'testing', '', 'testing some more', '15/Feb/2011 08:50:23'),
+(5, 3, 'testing', '', 'testing', '15/Feb/2011 08:53:16'),
+(6, 4, 'tesing', '', 'testing', '15/Feb/2011 08:53:27'),
+(5, 5, '', '', 'Gadzooks.', '17/Feb/2011 07:35:18'),
+(7, 6, 'Chaylo', '', 'test', '25/Mar/2011 10:35:29'),
+(7, 7, 'Chaylo', '', 'woot', '25/Mar/2011 10:44:55'),
+(5, 8, 'claurino', '', 'test\n', '28/Mar/2011 03:12:05'),
+(5, 9, 'claurino', '', 'testing again', '28/Mar/2011 03:15:06'),
+(9, 12, 'claurino', '', 'woot?', '28/Mar/2011 03:37:23');
 
 -- --------------------------------------------------------
 
@@ -147,24 +170,28 @@ INSERT INTO `replies` (`thread_id`, `reply_id`, `author`, `a_email`, `body`, `da
 --
 
 CREATE TABLE IF NOT EXISTS `threads` (
-  `id` int(4) NOT NULL AUTO_INCREMENT,
-  `topic` varchar(255) NOT NULL DEFAULT '',
-  `body` longtext NOT NULL,
-  `author` varchar(65) NOT NULL DEFAULT '',
+  `thread_id` int(4) NOT NULL AUTO_INCREMENT,
+  `thread_topic` varchar(255) NOT NULL DEFAULT '',
+  `thread_body` longtext NOT NULL,
+  `thread_author` varchar(65) NOT NULL DEFAULT '',
   `email` varchar(65) NOT NULL DEFAULT '',
   `datetime` varchar(25) NOT NULL DEFAULT '',
   `view` int(4) NOT NULL DEFAULT '0',
-  `reply` int(4) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+  `num_replies` int(4) NOT NULL DEFAULT '0',
+  `group_id` int(100) NOT NULL,
+  `group_name` varchar(128) NOT NULL,
+  PRIMARY KEY (`thread_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
 
 --
 -- Dumping data for table `threads`
 --
 
-INSERT INTO `threads` (`id`, `topic`, `body`, `author`, `email`, `datetime`, `view`, `reply`) VALUES
-(5, 'First Forum Entry', 'Just a quick test to make sure everything is working', 'Chaylo', '', '15/Feb/2011 07:59:21', 0, 3),
-(6, 'Gotta check it all again!', 'Ugh', 'Chaylo', '', '15/Feb/2011 08:50:01', 0, 1);
+INSERT INTO `threads` (`thread_id`, `thread_topic`, `thread_body`, `thread_author`, `email`, `datetime`, `view`, `num_replies`, `group_id`, `group_name`) VALUES
+(5, 'First Forum Entry', 'Just a quick test to make sure everything is working', 'Chaylo', '', '15/Feb/2011 07:59:21', 0, 6, 1, ''),
+(6, 'Gotta check it all again!', 'Ugh', 'Chaylo', '', '15/Feb/2011 08:50:01', 0, 1, 1, ''),
+(7, 'testing 1 2 3', 'Hello?', 'Chaylo', '', '25/Mar/2011 10:35:22', 0, 2, 1, 'test'),
+(9, 'testing', 'smurF!', 'claurino', '', '28/Mar/2011 03:18:44', 0, 3, 1, 'test');
 
 -- --------------------------------------------------------
 
@@ -173,15 +200,63 @@ INSERT INTO `threads` (`id`, `topic`, `body`, `author`, `email`, `datetime`, `vi
 --
 
 CREATE TABLE IF NOT EXISTS `user` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `un` varchar(100) NOT NULL,
   `pw` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=25 ;
 
 --
 -- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`id`, `un`, `pw`) VALUES
-(0, 'cblair', 'SHI/hel7');
+(0, 'cblair', 'SHI/hel7'),
+(11, 'anilson', 'supersecurepassword1234!@#$'),
+(12, 'long3841', 'long3841'),
+(13, 'sbeddall', '0017761826'),
+(15, 'brett', 'brett256'),
+(17, 'timb', 'frost741'),
+(18, 'claurino', '22877822'),
+(19, 'lwegner', '7ndustr8'),
+(20, 'crempel', 'SHI/hel7'),
+(21, 'abhay', 'gusabhay'),
+(22, 'drj', 'SHI/hel7'),
+(23, 'test', 'test123'),
+(24, 'admin', 'SHI/hel7');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usergroup`
+--
+
+CREATE TABLE IF NOT EXISTS `usergroup` (
+  `uid` int(11) NOT NULL,
+  `gid` int(11) NOT NULL,
+  `perm` tinyint(4) NOT NULL,
+  KEY `uid` (`uid`),
+  KEY `gid` (`gid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `usergroup`
+--
+
+INSERT INTO `usergroup` (`uid`, `gid`, `perm`) VALUES
+(23, 1, 6),
+(23, 5, 1),
+(23, 7, 3),
+(0, 10, 7),
+(24, 10, 7);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `usergroup`
+--
+ALTER TABLE `usergroup`
+  ADD CONSTRAINT `usergroup_ibfk_1` FOREIGN KEY (`uid`) REFERENCES `user` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `usergroup_ibfk_2` FOREIGN KEY (`gid`) REFERENCES `ggroup` (`id`) ON DELETE CASCADE;
