d41239301c8b9500c68fc7c258a6662cf0f13b00
brett <brett@brett-desktop.(none)>
2011-04-14 19:58:35 -0700

    grouppage

--- php/http/system/application/views/grouppage.php
+++ php/http/system/application/views/grouppage.php
@@ -8,15 +8,30 @@ echo "<div class=\"update\">"
 
 <?php 
 if( $authed ){
-  echo "<u>All Groups</u><br>"
+ 
+  echo ""
+    . "<table><tr>" 
+    . "<td><u>My Groups</u><br>"
     ;
- foreach( $groups as $group ):{
-    //Display all groups and link to their group/view.
-    echo anchor('grouppage/view/'.$this->Group->get_id($group) , $group)
-      ."<br>"
+ foreach( $grouplist as $key ):{
+    echo "<br>"
+      . anchor('grouppage/view/'.$key['gid'] , 
+	       $this->Group->get_name($key['gid']))
       ;
   }
   endforeach;
+  echo "</td>";
+  
+  echo "<td><u>All Groups</u><br>"
+    ;
+ foreach( $groups as $group ):{
+    //Display all groups and link to their group/view.
+    echo "<br>"
+      . anchor('grouppage/view/'.$this->Group->get_id($group) , $group)
+      ;
+  }
+  endforeach;
+  echo "</td></tr></table>";
  }
  else{
    echo "You must be logged in to view this page.";
