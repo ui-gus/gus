47079720a065e12fcd6af3963844cbb7040a9030
Alex Nilson <alexnilson@vandals.uidaho.edu>
2011-04-15 00:20:50 -0700

    Database integration, other good stuff

--- php/http/system/application/views/upload_form.php
+++ php/http/system/application/views/upload_form.php
@@ -2,11 +2,24 @@
 
 <?php 	echo $this->pdata['content'] ?>
 
-<?php echo $error;?>
+<?php echo $error['error']; ?>
 
-<?php echo form_open_multipart('upload/do_upload');?>
+<?php echo form_open_multipart('upload/do_upload'); ?>
 
 <input type="file" name="userfile" size="20" />
+
+<select name="groups">
+
+<?php 
+//Generate a drop down menu for group selection
+
+	foreach ( $grouplist as $key )
+	{
+		echo "<option value=\"" . $key['gid'] . "\">" . $this->Group->get_name($key['gid']) . "</option>";
+	}
+?>
+
+</select>
 
 <br /><br />
 
