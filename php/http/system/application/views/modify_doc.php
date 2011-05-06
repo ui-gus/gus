3ee76aeb02d19d6ac5c0ec9fd8c39fb4aa3cd294
Alex Nilson <alexnilson@vandals.uidaho.edu>
2011-05-05 17:16:00 -0700

    Added a feature that prevents users from trying to modify non-existant documents

--- php/http/system/application/views/modify_doc.php
+++ php/http/system/application/views/modify_doc.php
@@ -5,23 +5,27 @@
 
 
 <?php
-	echo form_open('docs/do_modify');
-?>
-<!--
-File name<br>!-->
-<?php
-	//echo "<input type=\"text\" name=\"new_name\" value=\"" . $_POST['file'] . "\" /> <br>";
-	echo "<input type=\"hidden\" name=\"file\" value=\"" . $_POST['file'] . "\" /> <br>";
-	echo $_POST['file'] . "<br>";
-?>
+	if(!isset($_POST['file']))
+	{
+		echo "You didn't select a file to modify.<br>";
+	}
+	else
+	{
+		echo form_open('docs/do_modify');
 
-Change file permissions<br>
-<input type="radio" name="perm" value="7" CHECKED/> Admin only<br>
-<input type="radio" name="perm" value="4"/> Officer and higher<br>
-<input type="radio" name="perm" value="0"/> Everyone<br>
-<input type="submit" value="Submit" />
-<?php
-	echo form_close();
+		//echo "<input type=\"text\" name=\"new_name\" value=\"" . $_POST['file'] . "\" /> <br>";
+		echo "<input type=\"hidden\" name=\"file\" value=\"" . $_POST['file'] . "\" /> <br>";
+		echo $_POST['file'] . "<br>";
+
+		echo "Change file permissions<br>";
+		echo "<input type=\"radio\" name=\"perm\" value=\"7\" CHECKED/> Admin only<br>";
+		echo "<input type=\"radio\" name=\"perm\" value=\"4\"/> Officer and higher<br>";
+		echo "<input type=\"radio\" name=\"perm\" value=\"0\"/> Everyone<br>";
+		echo "<input type=\"submit\" value=\"Submit\" />";
+
+		echo form_close();
+	}
+	echo "<a href=" . base_url() . "index.php/docs>Return to docs</a><br>";
 ?>
 
 <p>Page rendered in {elapsed_time} seconds</p>
