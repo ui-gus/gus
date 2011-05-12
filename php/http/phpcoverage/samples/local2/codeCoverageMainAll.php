5b714ecd35de89bba25cf1d02669d3724aa492cd
Colby Blair <cblair@vandals.uidaho.edu>
2011-05-12 13:42:33 -0700

    Added final tests, phpcoverage reports, and phpdocs.

---
+++ php/http/phpcoverage/samples/local2/codeCoverageMainAll.php
@@ -0,0 +1,43 @@
+<?php
+/*
+ *  $Id: codeCoverageMain.php 49493 2006-04-08 00:16:04Z hkodungallur $
+ *  
+ *  Copyright(c) 2004-2006, SpikeSource Inc. All Rights Reserved.
+ *  Licensed under the Open Software License version 2.1
+ *  (See http://www.spikesource.com/license.html)
+ */
+?>
+<?php
+    /**
+     * This file should be executed with following command
+     * Linux:
+     *   $ (export PHPCOVERAGE_HOME=/path/to/phpcoverage/src; php codeCoverageMain.php)
+     * Windows: 
+     *   > set PHPCOVERAGE_HOME=/path/to/phpcoverage/src
+     *   > php codeCoverageMain.php
+     *
+     */
+
+?>
+<?php
+    #require_once "phpcoverage.inc.php";
+    define("PHPCOVERAGE_HOME", "/var/git/gus-dev/php/http/phpcoverage/src");
+    require_once PHPCOVERAGE_HOME . "/CoverageRecorder.php";
+    require_once PHPCOVERAGE_HOME . "/reporter/HtmlCoverageReporter.php";
+
+    $reporter = new HtmlCoverageReporter("Code Coverage Report", "", "report");
+
+    #$includePaths = array(".");
+    #$excludePaths = array("codeCoverageMain.php", "test_driver.php");
+    $includePaths = array("/var/git/gus-dev/php/http/system/application/controllers");
+    $excludePaths = array(""); 
+    $cov = new CoverageRecorder($includePaths, $excludePaths, $reporter);
+    $cov->startInstrumentation();
+    #include "test_driver.php";
+    include "/var/git/gus-dev/php/http/index.php";
+    
+    $cov->stopInstrumentation();
+
+    $cov->generateReport();
+    $reporter->printTextSummary();
+?>
---
+++ php/http/phpcoverage/samples/local2/sample.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ *  $Id: sample.php 49493 2006-04-08 00:16:04Z hkodungallur $
+ *  
+ *  Copyright(c) 2004-2006, SpikeSource Inc. All Rights Reserved.
+ *  Licensed under the Open Software License version 2.1
+ *  (See http://www.spikesource.com/license.html)
+ */
+?>
+<?php
+	include "/var/git/gus-dev/php/http/system/libraries/Controller.php";
+	include "/var/git/gus-dev/php/http/system/application/controllers/home.php";
+	$h1 = new Home();
+
+    	$h1->test();
+
+?>
